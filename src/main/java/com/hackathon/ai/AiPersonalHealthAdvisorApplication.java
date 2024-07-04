fe849f800b020d72cdfdc16d3dc12c49d838567c
nitheshkumarn <nitheshkn012@gmail.com>
2024-07-04 09:38:32 +0530

    Basic maven structure with pom dependency

---
+++ src/main/java/com/hackathon/ai/AiPersonalHealthAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPersonalHealthAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPersonalHealthAdvisorApplication.class, args);
+	}
+
+}
